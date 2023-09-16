d5e4fe427aed76790ee1d42fcac4c5a6daa332c1
Jonas Emanuel <me@jonasemanuel.com>
2023-09-16 12:30:17 -0300

    feat: add users endpoint

--- UsersAPI/Program.cs
+++ UsersAPI/Program.cs
@@ -16,7 +16,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/users", () =>
 {
     return Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
