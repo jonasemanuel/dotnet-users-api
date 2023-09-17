eec3f191c5c239d0141b45e81381f32a2886a8df
Jonas Emanuel <me@jonasemanuel.com>
2023-09-17 20:01:42 -0300

    fix: adjust healthcheck

--- UsersAPI/Program.cs
+++ UsersAPI/Program.cs
@@ -1,8 +1,26 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks().AddNpgSql(
+         connectionString: builder.Configuration.GetConnectionString("Postgres"),
+         healthQuery: "SELECT 1",
+         name: "Database",
+         failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy
+    )
+    .AddUrlGroup(new Uri("http://httpbin.org/status/200"), "API Genérica com status code 200");
+
+builder.Services.AddHealthChecksUI(s =>
+{
+    s.AddHealthCheckEndpoint("Infnet API", "http://host.docker.internal:49155/healthz");
+})
+.AddInMemoryStorage();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
@@ -10,8 +28,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.MapControllers();
 }
+
+app.MapControllers();
+
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+
+       config.MapHealthChecksUI();
+   });
 
 
 app.Run();
